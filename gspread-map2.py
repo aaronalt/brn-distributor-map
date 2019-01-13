2619628f2cb6202c933b35a68e4e16ccad0cd6ed
brntechnology <46396052+brntechnology@users.noreply.github.com>
2019-01-13 22:11:12 +0300

    added layers for potential/current dist

--- gspread-map2.py
+++ gspread-map2.py
@@ -1,27 +1,50 @@
 import pandas as pd
 import folium 
 from folium.plugins import MarkerCluster
+#docs.google.com/spreadsheets/d/----->ID/export?format=csv&id=ID(replace 'ID' to export as csv)
+sheet_url = 'https://docs.google.com/spreadsheets/d/1BEXCT_VLp8FgnMxOHtblB7M7qArqthqqEYsSsL4mhZo/export?format=csv&id=1BEXCT_VLp8FgnMxOHtblB7M7qArqthqqEYsSsL4mhZo' 
 
-sheet_url = 'https://docs.google.com/spreadsheets/d/1BEXCT_VLp8FgnMxOHtblB7M7qArqthqqEYsSsL4mhZo/export?format=csv&id=1BEXCT_VLp8FgnMxOHtblB7M7qArqthqqEYsSsL4mhZo'
+#create a blank map, add feature groups for each layer
+m = folium.Map(location=[48.209619, 16.370439], zoom_start = 4, min_zoom = 2, tiles = "CartoDB positron", world_copy_jump = True, max_zoom = 15) 
+fg_potential = folium.FeatureGroup(name="Potential Distributors")
+fg_current = folium.FeatureGroup(name="Current Customers")
+#read csv and create dataframe
+df = pd.read_csv(sheet_url, delimiter=",", header=0, encoding='latin-1', engine='python') 
+#turn dataframe from dict into a tuple
+csv = [list(x) for x in df.values]
 
-df = pd.read_csv(sheet_url, delimiter=",", sep='\t', header=0, encoding='latin-1')
-
-csv = [tuple(x) for x in df.values]
-
-clean_data = [data for data in csv if not pd.isnull(data[7]) if not pd.isnull(data[4])]
-
-m = folium.Map(location=[38.4237, 27.1428], zoom_start = 5, min_zoom = 3, tiles = "CartoDB positron", world_copy_jump = True)
-
+#organize data into array
+    #location[14].strip(" ").split(";:,$./")
+    #notes = ' '.join(map(str, location[14]))
 locations = []
-for location in clean_data:
+for location in csv:
     coords = [location[0], location[1]]
-    noapostrophe = location[4].replace("'","")
-    locations.append({'company' : location[1], 'country' : location[2], 'products' : location[3], 'text': f"{location[3]}<br> {location[2]}<br> <a href={location[9]}>Website</a>", 'coordinates' : coords})
+    try:
+        locations.append({'company' : location[2], 'country' : location[3], 'products' : location[3], 'potential' : location[4], 'method' : location[13], 'note' : location[14], 'hover': f"{location[3]}<br>{location[2]}", 'click': f"<a href={location[9]}>Website</a><br>{location[13]}", 'coordinates' : coords})
+    except:
+        print("failed because of some error")
 
-cluster = MarkerCluster().add_to(m)
-
+#add marker clusters for grouping markers 
+current_cluster = MarkerCluster(options={'spiderfyOnMaxZoom' : True, 'disableClusteringAtZoom' : 8, 'zoomToBoundsOnClick' : False, 'spiderfyDistanceMultiplier' : 1.5})
+potential_cluster = MarkerCluster(options={'spiderfyOnMaxZoom' : True, 'disableClusteringAtZoom' : 8, 'zoomToBoundsOnClick' : False, 'spiderfyDistanceMultiplier' : 1.5}) 
+#iterate through the array, add marker for every distributor (row)
 for x in locations:
-    folium.Marker(location = (x['coordinates']), popup = x['text'], icon=folium.Icon(color='red')).add_to(cluster)
+    try:
+        if x['potential'] == "Current Customer":
+            folium.Marker(location = (x['coordinates']), popup = x['click'], tooltip = x['hover'], icon=folium.Icon(color='green')).add_to(current_cluster)
+        else:
+            folium.Marker(location = (x['coordinates']), popup = x['click'], tooltip = x['hover'], icon=folium.Icon(color='red')).add_to(potential_cluster)
+    except:
+        print("failed")
 
+#add feature groups to marker clusters, layer control for selecting layers
+fg_potential.add_child(potential_cluster)
+fg_current.add_child(current_cluster)
+m.add_child(fg_potential)
+m.add_child(fg_current)
+folium.LayerControl(position='topleft').add_to(m) 
+
+#save to html file
 m.save('index.html')
 
+ 
